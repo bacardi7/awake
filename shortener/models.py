fec770884b8c252e044db7b1e0e52ea3e0b06365
bacardi7 <holjjack7@gmail.com>
2022-09-25 23:44:29 +0900

    'PayPlan'

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class PayPlan(models.Model):
+    price = models.IntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+    create_at = models.DateTimeField(auto_now_add=True)
